aacedda238eca93859e53b9e253bfd9977ff86aa
Gail Foster <jumponcloud@aol.com>
2024-01-10 16:02:05 -0500

    Add test for compensation to DataBootstrapTest, update build.gradle

--- src/test/java/com/mindex/challenge/DataBootstrapTest.java
+++ src/test/java/com/mindex/challenge/DataBootstrapTest.java
@@ -1,14 +1,19 @@
 package com.mindex.challenge;
 
+import com.mindex.challenge.dao.CompensationRepository;
 import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Compensation;
 import com.mindex.challenge.data.Employee;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.junit.Assert.assertEquals;
 
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
@@ -18,13 +23,32 @@ public class DataBootstrapTest {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private CompensationRepository compensationRepository;
+
+    String testId;
+
+    @Before
+    public void setup() {
+        testId = "16a596ae-edd3-4847-99fe-c4518e82c86f";
+    }
+
     @Test
-    public void test() {
-        Employee employee = employeeRepository.findByEmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
+    public void employeeRepositoryTest() {
+        Employee employee = employeeRepository.findByEmployeeId(testId);
         assertNotNull(employee);
         assertEquals("John", employee.getFirstName());
         assertEquals("Lennon", employee.getLastName());
         assertEquals("Development Manager", employee.getPosition());
         assertEquals("Engineering", employee.getDepartment());
+    }
+
+    @Test
+    public void compensationRepositoryTest() {
+
+        Compensation actual = compensationRepository.findByEmployeeEmployeeId(testId);
+        assertNotNull(actual);
+        assertEquals(100000, actual.getSalary());
+        assertEquals(LocalDate.of(2024, 01, 31), actual.getEffectiveDate());
     }
 }
